9550f9f11edb11dc8debddfb478c37242e4df25e
aouos <aouos@qq.com>
2021-03-30 15:43:50 +0800

    update bubbleSort

--- src/components/bubbleSort.vue
+++ src/components/bubbleSort.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="bubble">
     <div class="anima">
+      <!-- Animation -->
       <div class="item" v-for="(item, index) in items" :key="index">
-        <span>{{ item }}</span>
+        <span class="item_top">{{ item }}</span>
         <span
           :class="[
             'box',
             {
-              jactive: j === index,
-              iactive: i === index,
-              gactive: index < items.length && index > i,
-              xactive: x === index,
+              jactive: index === points.j,
+              iactive: index === points.i,
+              gactive: index < items.length && index > points.i,
+              ractive: index === points.x,
             },
           ]"
           :style="{
@@ -20,18 +21,44 @@
         </span>
       </div>
     </div>
+
+    <!-- Pointer change  -->
     <div class="state">
       <div class="item state_box" v-for="(item, index) in items" :key="index">
         <span>
-          {{ index === x ? "☚" : "" }}
-          {{ index === i ? "i = " + i : "" }}
-          {{ index === j ? "j = " + j : "" }}
+          {{ index === points.x ? "☚" : "" }}
+          {{ index === points.i ? "i = " + points.i : "" }}
+          {{ index === points.j ? "j = " + points.j : "" }}
         </span>
       </div>
     </div>
+
+    <!-- Button group -->
     <div class="btn_group">
+      <button @click="delItems">删除元素</button>
+      <button @click="addItems">添加元素</button>
       <button @click="autoChange">自动演示</button>
       <button @click="nextChange">手动点击</button>
+    </div>
+
+    <!-- Tips -->
+    <div class="tips">
+      <div>
+        <span class="tips_block iactive"></span>
+        <span>变量i指向</span>
+      </div>
+      <div>
+        <span class="tips_block jactive"></span>
+        <span>变量j指向</span>
+      </div>
+      <div>
+        <span class="tips_block gactive"></span>
+        <span>排序完成</span>
+      </div>
+      <div>
+        <span class="tips_block ractive"></span>
+        <span>满足交换</span>
+      </div>
     </div>
   </div>
 </template>
@@ -39,55 +66,72 @@
 <script>
 import { bubbleFn } from "../library/bubbleSort.js";
 
+let genFn = null;
+
 export default {
   name: "bubbleSort",
   data() {
     return {
-      genFn: null,
       items: [3, 19, 6, 2, 8, 13, 0],
-      i: 0,
-      j: 0,
-      x: Infinity,
+      points: {
+        i: 0,
+        j: 0,
+        x: Infinity,
+      },
     };
   },
   mounted() {
-    this.genFn = bubbleFn(this.items);
-    this.nextChange();
+    this.initState();
   },
   methods: {
     nextChange() {
-      const res = this.genFn.next();
+      const res = genFn.next();
       if (!res.done) {
-        this.items = res.value.before;
-        this.i = res.value.i;
-        this.j = res.value.j;
-        if (res.value.isChange) {
-          this.x = this.j + 1;
-        }
-        setTimeout(() => {
-          this.items = res.value.current;
-          this.x = Infinity;
-        }, 1000);
+        this.change(res);
       }
     },
     autoChange() {
       let timer = setInterval(() => {
-        const res = this.genFn.next();
+        const res = genFn.next();
         if (!res.done) {
-          this.items = res.value.before;
-          this.i = res.value.i;
-          this.j = res.value.j;
-          if (res.value.isChange) {
-            this.x = this.j + 1;
-          }
-          setTimeout(() => {
-            this.items = res.value.current;
-            this.x = Infinity;
-          }, 1000);
+          this.change(res);
         } else {
           clearInterval(timer);
         }
       }, 2000);
+    },
+    change(res) {
+      this.items = res.value.before;
+      this.points.i = res.value.i;
+      this.points.j = res.value.j;
+      if (res.value.isChange) {
+        this.points.x = this.points.j + 1;
+      }
+      setTimeout(() => {
+        this.items = res.value.current;
+        this.points.x = Infinity;
+      }, 1000);
+    },
+    delItems() {
+      if (this.items.length > 3) {
+        this.items.pop();
+      }
+      setTimeout(() => {
+        this.initState();
+      }, 100);
+    },
+    addItems() {
+      if (this.items.length < 15) {
+        const temp = Math.floor(Math.random() * 20);
+        this.items.push(temp);
+      }
+      setTimeout(() => {
+        this.initState();
+      }, 100);
+    },
+    initState() {
+      genFn = bubbleFn(this.items);
+      this.nextChange();
     },
   },
   watch: {},
@@ -98,6 +142,7 @@ export default {
 .bubble {
   width: 100%;
   text-align: center;
+  user-select: none;
 }
 
 .anima,
@@ -111,18 +156,19 @@ export default {
   flex-direction: column;
   justify-content: flex-end;
   overflow: hidden;
-  width: 3.6em;
-  margin: 0 0.3em;
+  width: 4em;
+  margin: 0 0.5em;
 }
 
 .item span {
   display: block;
-  padding: 0.1em;
-  width: 3.6em;
+  padding: 1px 0;
+  width: 100%;
+  border-radius: 0.2em;
 }
 
 .box {
-  background-color: #eee;
+  background-color: rgb(246, 248, 250);
 }
 
 .state {
@@ -130,45 +176,70 @@ export default {
 }
 
 .state_box {
-  background-color: #f1f1f1;
+  background-color: rgb(246, 248, 250);
   color: #333;
-  border-radius: 0.2em;
+  padding: 0.2em 0;
+  text-align: center;
 }
 
 .iactive {
-  background-color: yellow;
+  background-color: rgb(121, 184, 255);
 }
 
 .jactive {
-  background-color: pink;
+  background-color: rgb(241, 224, 90);
 }
 
 .gactive {
-  background-color: green;
+  background-color: rgb(46, 164, 79);
 }
 
-.xactive {
-  background-color: red;
+.ractive {
+  background-color: rgb(227, 76, 38);
 }
 
 .iactive,
 .jactive,
 .gactive {
-  color: #fff;
-  transition: background 0.5s ease-in-out;
+  transition: background 0.5s ease-out;
 }
 
-.btn_group,
-.source {
+.btn_group {
   margin-top: 1.8em;
 }
 
 button {
-  background-color: plum;
+  background-color: rgb(36, 41, 46);
   padding: 0.8em 1.5em;
   border: 0;
   border-radius: 0.5em;
   outline: none;
   color: #fff;
+  cursor: pointer;
+}
+
+button:active {
+  transform: scale(0.9);
+  transition: all 0.5s ease-out;
+}
+
+.tips {
+  position: fixed;
+  right: 1em;
+  top: 1em;
+  text-align: left;
+}
+
+.tips div {
+  display: flex;
+  align-items: center;
+}
+
+.tips_block {
+  display: inline-block;
+  width: 2em;
+  height: 2em;
+  margin: 0.2em;
+  border-radius: 0.2em;
 }
 </style>
