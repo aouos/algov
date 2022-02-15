7f115575dd960991240bf56c055fe4577445e926
aouos <aouos@qq.com>
2022-02-15 23:19:38 +0800

    refactor: init the project with vite & react

---
+++ .prettierrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // 一行最多 120 字符
+  printWidth: 120,
+  // 使用 2 个空格缩进
+  tabWidth: 2,
+  // 不使用缩进符，而使用空格
+  useTabs: false,
+  // 行尾需要有分号
+  semi: true,
+  // 使用单引号
+  singleQuote: true,
+  // jsx 不使用单引号，而使用双引号
+  jsxSingleQuote: false,
+  // 末尾需要有逗号
+  trailingComma: 'all',
+  // 大括号内的首尾需要空格
+  bracketSpacing: true,
+  // jsx 标签的反尖括号需要换行
+  jsxBracketSameLine: false,
+  // 箭头函数，只有一个参数的时候，也需要括号
+  arrowParens: 'always',
+  // 换行符使用 lf
+  endOfLine: 'lf',
+};
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# anima
-
-main 分支与页面暂停更新
-
-将在 next 分支重构项目
+# Algorithm animation
